af75174a5705f42134660299b8a8aeed1558289c
de9 <fxpereira@gmx.com>
2018-12-27 20:15:05 +0100

    añadido videotutorial03

--- src/videotutorial03/Bisiesto.java
+++ src/videotutorial03/Bisiesto.java
@@ -1,9 +1,33 @@
 package videotutorial03;
 
+import entradaDatos.*;
+import java.io.*;
+
+
 public class Bisiesto {
 
 	public static void main(String[] args) {
-
+		//Decalaración de variable
+		
+		int año=0;
+		boolean bisiesto = false;
+		
+		//Entrada de datos
+		
+		System.out.print("Introduzca el año deseado: ");
+		año = Entrada.leerEntero();
+		
+		//Cálculos
+		
+		bisiesto = ((año%4 == 0) && (año%100) != 0) || (año%400 == 0);
+		
+		//Salida de resultados
+		
+		if(bisiesto == true) {
+			System.out.println("El año "+año+" es bisiesto");
+		}else {
+			System.out.println("El año "+año+" no es bisiesto");
+		}
 	}
 
 }

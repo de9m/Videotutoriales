3e9e15eee0876bf1b0db02b139c23f2519308761
de9 <fxpereira@gmx.com>
2018-12-28 16:30:10 +0100

    añadido videotutorialescerocuatro

---
+++ src/videotutorialcerocuatro/Mensaje.java
@@ -0,0 +1,18 @@
+package videotutorialcerocuatro;
+
+
+public class Mensaje {
+
+	public static void main(String[] args) {
+		
+		mostrarMensaje("Bienvenida");
+		
+		mostrarMensaje("Despedida");
+
+	}
+	
+	private static void mostrarMensaje(String mensaje){
+		
+		System.out.println("***"+mensaje+"***");
+	}
+}
